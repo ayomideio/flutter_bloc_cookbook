1cfa633ee0c080b612a221a3678384add2634cc7
ayomideio <gokeayomide.tolu@gmail.com>
2023-07-05 23:29:08 +0100

    complete

--- bin/flutter_bloc_cookbook.dart
+++ bin/flutter_bloc_cookbook.dart
@@ -1,20 +1,25 @@
 import 'package:args/args.dart';
-import 'package:flutter_bloc_cookbook/app-generator.dart';
 
+import 'package:flutter_bloc_cookbook/app-generator.dart';
 
 void main(List<String> arguments) async {
   final parser = ArgParser();
-  parser.addOption('name',
-      abbr: 'n', help: 'The name of the Flutter app to create.');
+  parser.addCommand('create', _createCommand);
 
-  final argResults = parser.parse(arguments);
-
-  final appName = argResults['name'] as String?;
-  if (appName == null) {
-    print(
-        'Error: The name of the app is required. Usage: create_flutter_app -n <app_name>');
-    return;
+  final args = parser.parse(arguments);
+  final command = args.command;
+  if (command?.name == 'create') {
+    final blocName = command?.rest?.first;
+    if (blocName == null) {
+      print('Error: Please provide a Bloc name. Usage: flutter_bloc_cookbook create <bloc_name>');
+      return;
+    }
+    await AppGenerator.generateSampleApp(blocName);
+    print('Successfully created the Bloc: $blocName');
+  } else {
+    print('Invalid command. Usage: flutter_bloc_cookbook create <bloc_name>');
   }
-
-  await AppGenerator.generateSampleApp(appName);
 }
+
+final _createCommand = ArgParser()
+  ..addOption('bloc_name', help: 'Name of the Bloc to create');
